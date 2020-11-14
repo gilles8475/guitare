3e92d1b3f6bb17a466b59c88b0d1566f6a1c7fa6
gilles7584 <gilldupont@gmail.com>
2020-11-14 10:34:30 +0100

    modifications du fichier readme et mise en forme des boutons

--- JS/script.js
+++ JS/script.js
@@ -34,7 +34,13 @@ function createFormulaire(){
   var bout1=document.createElement("button");
 
   bout.innerHTML= "Marque";
-  bout1.innerHTML= "Intervales";
+  bout1.innerHTML= "Affiche intervalles";
+  bout.style.backgroundColor='#8B0000';
+  bout.style.color='white';
+  bout.style.width='20%';
+  bout1.style.width='20%';
+  bout1.style.backgroundColor='#8B0000';
+  bout1.style.color='white';
   el.appendChild(bout);
   el.appendChild(bout1);
   bout.onclick= function(event,form=newFormulaire, manch=newManche){ marque(form,manch);};
@@ -48,58 +54,6 @@ function createFormulaireText(){
   titre.innerHTML= "Exercice "+(1+Manche.NbInstance++);
   document.getElementById('main').appendChild(titre);
   var newFormulaireText= new FormulaireText(20);
-  /*
-  document.onkeypress=function(event,frm=newFormulaireText){
-    frm.toggle();
-  }*/
+
 
 }
-
-//test audio------------------------
-/*
-var audioCtx;
-var oscillator;
-
-function joueAudio(wavefrm){
-
-  audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  oscillator = audioCtx.createOscillator();
-  const gainNode = audioCtx.createGain();
-  gainNode.gain.value=0.05;
-  //create custom wave form
-  var real = new Float32Array(7);
-  var imag = new Float32Array(7);
-  real[0] = 0;
-  imag[0] = Math.random();
-  real[1] = Math.random();//coef sur le premiere hamonique
-  imag[1] = Math.random();
-  real[2] = Math.random();//coef sur la deuxieme harmonique
-  imag[2] = Math.random();
-  real[3] = Math.random();//etc ...
-  imag[3] = Math.random();
-  real[4] = Math.random();
-  imag[4] = Math.random();
-  real[5] = Math.random();
-  imag[5] = Math.random();
-  real[6] = Math.random();
-  imag[6] = Math.random();
-
-  var wave = audioCtx.createPeriodicWave(real, imag, {disableNormalization: true});
-  oscillator.setPeriodicWave(wave);
-
-// create Oscillator node-------------------
-
-    //oscillator.type = wavefrm;
-    //oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
-
-    oscillator.connect(gainNode).connect(audioCtx.destination);
-    oscillator.start();
-    oscillator.stop(2);
-}
-
-function stopAudio(){
-  oscillator.stop();
-}
-//oscillator.start();
-//-----------------
-*/
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ plot guitar notes chords intervals and record on a formular
 simulation manche de guitare avec formulaires pour inscrire les notes jouées sous forme corde/case.
 
 Appuyer sur le bouton 'Nouveau formulaire avec manche' pour afficher un manche de guitare. on joue une case en cliquant sur la case. si on clique à nouveau sur cette case, la note est enlevée. Si on veut que la note soit une fondamentale on clique en appuyant sur la touche CTRL.
-On visualise les intervalles par rapport à la fondamentale en appuyant sur le bouton 'Intervales'. Les notes jouées peuvent être enregistrées sur le formulaire en appuyant sur le bouton 'Marque'. L'appui sur le bouton 'Marque' affiche les notes du manche sur le formulaire vide le manche et passe à la case suivante du formulaire.
+On visualise les intervalles par rapport à la fondamentale en appuyant sur le bouton 'Affiche intervalles'. Les notes jouées peuvent être enregistrées sur le formulaire en appuyant sur le bouton 'Marque'. L'appui sur le bouton 'Marque' affiche les notes du manche sur le formulaire, vide le manche et passe à la case suivante du formulaire.
 On peut revenir sur une case du formulaire en cliquant dessus. les notes enregistrées s'affichent alors sur le manche.
 On crée autant de formulaire que l'on veut en cliquant sur le menu 'Nouveau formulaire avec manche'.
 
